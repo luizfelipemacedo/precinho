65b9081985c17095445efa17b354239765fda5dd
Luiz Macedo <felipemacedoc89@gmail.com>
2023-06-17 22:42:44 -0300

    refactor: remove função que não está sendo utilizada

--- src/scripts/fetchData.js
+++ src/scripts/fetchData.js
@@ -1,6 +1,6 @@
 export async function fetchData() {
     if (!sessionStorage.getItem("data")) {
-        const res = await fetch("../../../data/data.json");
+        const res = await fetch("../../../data/products.json");
         const data = await res.json();
         sessionStorage.setItem("data", JSON.stringify(data));
         return data;
@@ -9,41 +9,6 @@ export async function fetchData() {
         return JSON.parse(data);
     }
 }
-
-export function handleServiceWorker() {
-    navigator.serviceWorker.register('/serviceWorker.js')
-    .then(async serviceWorker => {
-        serviceWorker.update()
-        try {
-            let subscription = await serviceWorker.pushManager.getSubscription()
-
-        if (!subscription) {
-            const publicKeyResponse = await fetch('http://localhost:3005/notifications/push/public-key')
-            const data = await publicKeyResponse.json();
-            subscription = await serviceWorker.pushManager.subscribe({
-                applicationServerKey: await data.publicKey,
-                userVisibleOnly: true
-            })
-        }
-        console.log(subscription);
-
-        await fetch('http://localhost:3005/notifications/push/send', {
-            method: "POST",
-            body: JSON.stringify({ subscription }),
-            headers: {
-                "Accept": "*/*",
-                "Content-Type": "application/json"
-               }
-            ,
-            mode: "cors"
-        })
-
-        } catch (error) {
-            console.log(error);   
-        }
-    })
-}
-
 
 export async function fetchCoord() {
     if (!sessionStorage.getItem("market_coord")) {
